e92efe5f52642fd1f7618499b4a689e0ed9944ce
Kendall Rogers <kendall.rogers@chartboost.com>
2023-08-29 14:53:13 -0700

    Add support for fixed size banners inside adaptive banner placements

--- Source/HyprMXAdapter.swift
+++ Source/HyprMXAdapter.swift
@@ -150,7 +150,12 @@ final class HyprMXAdapter: PartnerAdapter {
         case .rewarded:
             return HyprMXAdapterRewardedAd(adapter: self, request: request, delegate: delegate)
         default:
-            throw error(.loadFailureUnsupportedAdFormat)
+            // Not using the `.adaptiveBanner` case directly to maintain backward compatibility with Chartboost Mediation 4.0
+            if request.format.rawValue == "adaptive_banner" {
+                return HyprMXAdapterBannerAd(adapter: self, request: request, delegate: delegate)
+            } else {
+                throw error(.loadFailureUnsupportedAdFormat)
+            }
         }
     }
 
