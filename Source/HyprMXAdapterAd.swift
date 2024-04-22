630b3938db802979e681817e421fdc0f85a3ae7a
Daniel Barros <dani.barros@me.com>
2024-04-22 23:35:23 +0530

    Split PartnerAd into banner and fullscreen (#26)
    * Split makeAd functions
    
    * Update show method
    
    * Update banners conformance
    
    * Fix linting issues
    
    * Remove unnecessary return keywords

--- Source/HyprMXAdapterAd.swift
+++ Source/HyprMXAdapterAd.swift
@@ -10,14 +10,6 @@ import HyprMX
 /// Base class for Chartboost Mediation HyprMX adapter ads.
 /// It is convenient to create a base class for your adapter's PartnerAd types, although this is not required.
 class HyprMXAdapterAd: NSObject {
-    /// The partner ad view to display inline. E.g. a banner view.
-    /// Should be nil for full-screen ads.
-    var inlineView: UIView?
-
-    /// The loaded partner ad banner size.
-    /// Should be `nil` for full-screen ads.
-    var bannerSize: PartnerBannerSize?
-
     /// The partner adapter that created this ad.
     let adapter: PartnerAdapter
 
@@ -25,11 +17,13 @@ class HyprMXAdapterAd: NSObject {
     var details: PartnerDetails = [:]
 
     /// The ad load request associated to the ad.
-    /// It should be the one provided on `PartnerAdapter.makeAd(request:delegate:)`.
+    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)``
+    /// or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
     let request: PartnerAdLoadRequest
 
     /// The partner ad delegate to send ad life-cycle events to.
-    /// It should be the one provided on `PartnerAdapter.makeAd(request:delegate:)`.
+    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)``
+    /// or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
     weak var delegate: PartnerAdDelegate?
 
     /// The completion for the ongoing load operation.
