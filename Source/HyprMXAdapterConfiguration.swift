ec47684410cb6722ad50d8fd02f0fe410a7af161
Daniel Barros <dani.barros@me.com>
2024-04-26 12:25:00 -0700

    HB-7647: Configuration class rework (#28)
    * Relocate adapter properties to configuration
    
    * Expand configuration options
    
    * Add public keyword
    
    * Use actions v2 with support for relocated partnerVersion definition
    
    * Add missing import

---
+++ Source/HyprMXAdapterConfiguration.swift
@@ -0,0 +1,38 @@
+// Copyright 2022-2024 Chartboost, Inc.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+import Foundation
+import HyprMX
+import os.log
+
+/// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
+@objc public class HyprMXAdapterConfiguration: NSObject {
+
+    /// The version of the partner SDK.
+    @objc public static var partnerSDKVersion: String {
+        HyprMX.versionString()
+    }
+
+    /// The version of the adapter.
+    /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
+    /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
+    @objc public static let adapterVersion = "4.6.3.0.0"
+
+    /// The partner's unique identifier.
+    @objc public static let partnerID = "hyprmx"
+
+    /// The human-friendly partner name.
+    @objc public static let partnerDisplayName = "HyprMX"
+
+    /// Flag that can optionally be set to change the log level of the HyprMX SDK.
+    @objc public static var logLevel: HYPRLogLevel = HYPRLogLevelError {
+        didSet {
+            HyprMX.setLogLevel(logLevel)
+            os_log(.debug, log: log, "HyprMX SDK log level set to %{public}s", "\(logLevel)")
+        }
+    }
+
+    private static let log = OSLog(subsystem: "com.chartboost.mediation.adapter.hyprmx", category: "Configuration")
+}
